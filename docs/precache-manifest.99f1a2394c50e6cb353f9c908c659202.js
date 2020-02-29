3159f41440d863f9c4381e69bc81fee063f2f72c
Meet Patel <46988084+meetpatel5720@users.noreply.github.com>
2020-02-29 17:24:43 +0530

    Initial commit

---
+++ docs/precache-manifest.99f1a2394c50e6cb353f9c908c659202.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = [
+  {
+    "revision": "8db216fe70429724fa1d970151db1b3e",
+    "url": "/img/Auto_Motive_Philosopher.8db216fe.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "893745595c83e1208bd130b8ac6916e1",
+    "url": "/img/9-3.89374559.png"
+  },
+  {
+    "revision": "ae2e5c7dacf326bfd3f7",
+    "url": "/css/chunk-vendors.6b0c58bc.css"
+  },
+  {
+    "revision": "3e206e7186c3341045a7e87ebbe95172",
+    "url": "/img/magic-flame-effect.3e206e71.png"
+  },
+  {
+    "revision": "d04f031565b43dd85818a430c8198a1c",
+    "url": "/img/scamandersSuitcase.d04f0315.png"
+  },
+  {
+    "revision": "78bfda3c79aaca12d935f6b8bffbc763",
+    "url": "/img/d5.78bfda3c.png"
+  },
+  {
+    "revision": "c9e56fbbdce5bbc2b2bf316ba681389d",
+    "url": "/img/1.c9e56fbb.png"
+  },
+  {
+    "revision": "1c61919a868476117a3c425b8ee82e3b",
+    "url": "/img/background.1c61919a.jpg"
+  },
+  {
+    "revision": "4fd495c484808d58deeb7438d6f6ff48",
+    "url": "/img/page2.4fd495c4.jpg"
+  },
+  {
+    "revision": "202e23f4a87ad5ea9830b8e20284c49f",
+    "url": "/img/9.202e23f4.png"
+  },
+  {
+    "revision": "3458fba5abb60517f7b03f4f48609674",
+    "url": "/img/gobletOfFireBg.3458fba5.jpg"
+  },
+  {
+    "revision": "43a8cf12d4fe667aff8a264be04a131d",
+    "url": "/img/d3.43a8cf12.png"
+  },
+  {
+    "revision": "8d4a0572da1883f33f64765491748532",
+    "url": "/img/page.8d4a0572.png"
+  },
+  {
+    "revision": "d3b542a2095dfb2aece11f35c423a48e",
+    "url": "/img/6.d3b542a2.png"
+  },
+  {
+    "revision": "de2fa53b146ccc47bf1b93115f6b36f2",
+    "url": "/fonts/muggleNews.de2fa53b.ttf"
+  },
+  {
+    "revision": "1db8c3a84e1e1b78a1858360922bef15",
+    "url": "/fonts/headlineOne.1db8c3a8.TTF"
+  },
+  {
+    "revision": "75673e5ff5e07bee6c13b0f60fd694b2",
+    "url": "/fonts/dum1.75673e5f.ttf"
+  },
+  {
+    "revision": "2fb4958dcb8af379b9d53f345ba57ffa",
+    "url": "/fonts/HalfBloodPrince.2fb4958d.ttf"
+  },
+  {
+    "revision": "e7cc38f2071b065129f170093ad0cfa1",
+    "url": "/fonts/MagicSchoolOne.e7cc38f2.ttf"
+  },
+  {
+    "revision": "8f3efa9a61ac8f697b68d7c825e649a6",
+    "url": "/fonts/AnimalesFantastic.8f3efa9a.otf"
+  },
+  {
+    "revision": "4834f12f4baeb6cccc0a01e0aa35d99a",
+    "url": "/fonts/ParryHotter.4834f12f.ttf"
+  },
+  {
+    "revision": "ced0b95e15c82cceeba3a2994ee7dd1c",
+    "url": "/img/deathlyHollows.ced0b95e.jpg"
+  },
+  {
+    "revision": "70569f2dbe884708e5bfde32bff00176",
+    "url": "/img/M.A.D._Hollows.70569f2d.png"
+  },
+  {
+    "revision": "432636baad0b14a3e683a5f6a30776a1",
+    "url": "/img/Advanced_Potion_Making.432636ba.png"
+  },
+  {
+    "revision": "b03b63ac81bdfc46d37b00ad154fb57e",
+    "url": "/img/halfWavePrince.b03b63ac.png"
+  },
+  {
+    "revision": "a4706df2ba64678c141c5f601f1c04dd",
+    "url": "/img/d6.a4706df2.png"
+  },
+  {
+    "revision": "e2fe46813d90e15ce0c879e36276ba99",
+    "url": "/img/d2.e2fe4681.png"
+  },
+  {
+    "revision": "b204e2d30d817e6994ffd6da841ee218",
+    "url": "/img/suitcaseTop.b204e2d3.png"
+  },
+  {
+    "revision": "10c26492c28dcf3dc9829cc174b58b44",
+    "url": "/img/udaan.10c26492.gif"
+  },
+  {
+    "revision": "36ab500713924ffec47e39df71683576",
+    "url": "/img/walk.36ab5007.png"
+  },
+  {
+    "revision": "1cccd4914dcbe904cdf1e643b4076f53",
+    "url": "/img/11.1cccd491.png"
+  },
+  {
+    "revision": "12af3ef3d643151f2e838daf58242165",
+    "url": "/img/fire-bird-phoenix-fantasy-creature-illustration2.12af3ef3.jpg"
+  },
+  {
+    "revision": "db3dcf530631abf18adaec4e019c2124",
+    "url": "/img/Scamander_Suitcase_s.db3dcf53.png"
+  },
+  {
+    "revision": "58f19e132552a3c1d037",
+    "url": "/js/app.c5af9387.js"
+  },
+  {
+    "revision": "ae2e5c7dacf326bfd3f7",
+    "url": "/js/chunk-vendors.6f4fe25c.js"
+  },
+  {
+    "revision": "c2e93cff064f75573cf7ca981647f408",
+    "url": "/img/Half_wave_Prince.c2e93cff.png"
+  },
+  {
+    "revision": "d5d39b03fed844ed5319a7f9cbcc1271",
+    "url": "/fonts/LUMOS.d5d39b03.TTF"
+  },
+  {
+    "revision": "6980f38e33279143821ca37f6b525b63",
+    "url": "/img/rain.6980f38e.png"
+  },
+  {
+    "revision": "86eb8929bdb5a735d6516f631d1615d1",
+    "url": "/img/goblet.86eb8929.png"
+  },
+  {
+    "revision": "6584800fac5a02d2d8772c1171f0bec7",
+    "url": "/img/smoke.6584800f.png"
+  },
+  {
+    "revision": "91f9bcbfff1c28f972fac5d637920df1",
+    "url": "/img/station.91f9bcbf.png"
+  },
+  {
+    "revision": "96688cd2596f865dfceff7a279e8eef8",
+    "url": "/img/Smoke30Frames_0.96688cd2.png"
+  },
+  {
+    "revision": "3c2d56587469c0ffaf1e7b68046a7d14",
+    "url": "/img/17.3c2d5658.png"
+  },
+  {
+    "revision": "1d6ef71bf1572bee5175f25e8fc28f08",
+    "url": "/img/9-1.1d6ef71b.png"
+  },
+  {
+    "revision": "143984d3eeba293f150a9cd831c99483",
+    "url": "/img/Chamber_of_coders.143984d3.png"
+  },
+  {
+    "revision": "7057a9509b1b2f6914df0c9f6c9e7977",
+    "url": "/img/d4.7057a950.png"
+  },
+  {
+    "revision": "d7bc260d3a9f5343011180232e5337b5",
+    "url": "/img/Order_of_OHMS.d7bc260d.png"
+  },
+  {
+    "revision": "3da891a069087edcda66ac0913f0c8ce",
+    "url": "/fonts/dum1ital.3da891a0.ttf"
+  },
+  {
+    "revision": "9c03db0ab9a6cec644ca26a17f810c9d",
+    "url": "/img/Azkaban_concept_art.9c03db0a.png"
+  },
+  {
+    "revision": "bc7cc57bdd9c724a8b3c1f439b039131",
+    "url": "/img/d7.bc7cc57b.png"
+  },
+  {
+    "revision": "b18a47a6ec9b22bb8e7d78bc87774309",
+    "url": "/img/teamBg.b18a47a6.jpg"
+  },
+  {
+    "revision": "ba18a9acbc902e45b033c231ee03600f",
+    "url": "/img/chamberOfCodersBg.ba18a9ac.jpg"
+  },
+  {
+    "revision": "c8f8eb23723e6dcdb9f4be208bf2b1ba",
+    "url": "/img/Azkaban_WB_F5_AzkabanExteriorEscape_Still_080615_Land.c8f8eb23.jpg"
+  },
+  {
+    "revision": "d98f58da5e961e4cfdfb2a2aa64971ea",
+    "url": "/img/instagram.d98f58da.png"
+  },
+  {
+    "revision": "18397f643eb46bdcca5f2657bdd7cd1c",
+    "url": "/img/wand.18397f64.png"
+  },
+  {
+    "revision": "a40c61e75b2dbcdab930d48be1b2d309",
+    "url": "/img/logo.a40c61e7.png"
+  },
+  {
+    "revision": "2298cb1b6d3ace9ab0d3238d3d5a0217",
+    "url": "/img/d1.2298cb1b.png"
+  },
+  {
+    "revision": "f24925f355722ea919ad27aa28512a4a",
+    "url": "/img/8.f24925f3.png"
+  },
+  {
+    "revision": "19ed0041bd5b4bbf19cc76bf4fee87e1",
+    "url": "/img/18.19ed0041.png"
+  },
+  {
+    "revision": "c3492d69d481c902d1efd4c060e6ef31",
+    "url": "/img/Builder_of_Azkaban.c3492d69.png"
+  },
+  {
+    "revision": "9612542ffd602270e113433ba5953821",
+    "url": "/img/suitcaseBottom.9612542f.png"
+  },
+  {
+    "revision": "05857735379156a07e457ab259a5482c",
+    "url": "/img/snape.05857735.svg"
+  },
+  {
+    "revision": "f8df21490b911ab243d0eb153b9d4f75",
+    "url": "/img/hat.f8df2149.png"
+  },
+  {
+    "revision": "ad9ecb7af2b9ecf45a62258de01a3661",
+    "url": "/img/nametag.ad9ecb7a.png"
+  },
+  {
+    "revision": "a37368ef456c220739f55245a2187e2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b69a0974484f26cb4de6842af1664dd",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "58f19e132552a3c1d037",
+    "url": "/css/app.1a82c1a3.css"
+  }
+];
